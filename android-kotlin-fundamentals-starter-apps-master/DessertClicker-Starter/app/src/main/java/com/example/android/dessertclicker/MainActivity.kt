32e5c3fa824a31482d19b4d678379b829f23cda4
KikiLiii <kate95761@gmail.com>
2023-04-02 13:51:37 +0300

    create project

--- android-kotlin-fundamentals-starter-apps-master/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ android-kotlin-fundamentals-starter-apps-master/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -21,6 +21,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
